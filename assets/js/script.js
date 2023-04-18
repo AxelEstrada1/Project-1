abcaa3d72afb9e3bdf025cfbff168f5afe1df93c
jgum63 <joshgum3@gmail.com>
2023-04-18 12:53:41 -0500

    updated body

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,15 +11,19 @@ fetch('https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/', options)
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
-    const option = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '37f0117021msh3bf81e73c9ea30ap12d251jsn858668b3e15e',
-            'X-RapidAPI-Host': 'astronomy.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://astronomy.p.rapidapi.com/api/v2/bodies/positions?latitude=33.775867&longitude=-84.39733&from_date=2017-12-20&to_date=2017-12-21&elevation=166&time=12%3A00%3A00', option)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+
+
+const option = {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': '37f0117021msh3bf81e73c9ea30ap12d251jsn858668b3e15e',
+    'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
+  },
+  body: '{"model":"gpt-3.5-turbo","messages":[{"role":"user","content":"Hello!"}]}'
+};
+  
+fetch('https://openai80.p.rapidapi.com/chat/completions', option)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
