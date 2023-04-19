a94f603311213f1367c6e01dc80a4c7edbc06321
Dawn Kline <dawn.kline.m@gmail.com>
2023-04-19 11:35:18 -0500

    updates it js

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,54 @@ fetch('https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/', options)
 // 	.then(response => response.json())
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
+
+// fetch('https://covers.openlibrary.org/b/id/12547191.json')
+//     .then(response => response.json())
+//     .then(libraryData => {
+
+//         console.log(libraryData);
+//     })
+
+
+setImage();
+async function fetchImage(url) {
+    const img = new Image();
+    return new Promise((res, rej) => {
+        img.onload = () => res(img);
+        img.onerror = e => rej(e);
+        img.src = url;
+    });
+}
+
+async function setImage() {
+
+    const img = await fetchImage('https://covers.openlibrary.org/b/id/12547191-L.jpg');
+    const w = img.width;
+    const h = img.height;
+    document.body.appendChild(img)
+    console.log(img)
+
+}
+// async function fetchImage(url) {
+//     const img = await new Image();
+//     return new Promise((res, rej) => {
+//         img.onload = () => res(img);
+//         img.onerror = e => rej(e);
+//         img.src = url;
+//     });
+// }
+// const img = fetchImage('https://covers.openlibrary.org/b/id/12547191-L.jpg');
+// console.log(img.url)
+// const w = img.width;
+// const h = img.height;
+// fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings ")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+
+fetch("https://openlibrary.org/search.json?q=jupiter ")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
