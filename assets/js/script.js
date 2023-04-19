5e7318f8a0e190d0fa859ec4db289ebcb4e79821
jgum63 <joshgum3@gmail.com>
2023-04-19 11:33:28 -0500

    updated script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '0bb9ee6632mshc5ac8ef7fdd663fp153f7bjsnd8f9747790b0',
-        'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
-    }
-};
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': '0bb9ee6632mshc5ac8ef7fdd663fp153f7bjsnd8f9747790b0',
+//         'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
+//     }
+// };
 
-fetch('https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+// fetch('https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
 
 // const options = {
 // 	method: 'POST',
@@ -26,3 +26,40 @@ fetch('https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/', options)
 // 	.then(response => response.json())
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
+
+
+
+// function getPlanetData(planetName) {
+//     const options = {
+//       method: 'GET',
+//       headers: {
+//         'X-RapidAPI-Key': '0bb9ee6632mshc5ac8ef7fdd663fp153f7bjsnd8f9747790b0',
+//         'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
+//       }
+//     };
+  
+//     return fetch(`https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/${planetName}`, options)
+//       .then(response => response.json())
+//       .then(data => data)
+//       .catch(err => console.error(err));
+//   }
+  
+//   function handleButtonClick(event) {
+//     const planetName = event.target.textContent.toLowerCase();
+  
+//     if (localStorage.getItem(planetName)) {
+//       alert(`You have already clicked on the ${planetName} planet.`);
+//     } else {
+//       localStorage.setItem(planetName, true);
+//       getPlanetData(planetName).then(planetData => {
+//         displayPlanetDetails(planetData);
+//       });
+//     }
+//   }
+  
+
+  fetch("https://openlibrary.org/search.json?q=jupiter ")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
